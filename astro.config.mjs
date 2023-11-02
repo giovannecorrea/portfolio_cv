fff591751d27acad7a7ec965d413b8811e660144
Giovanne Corrêa <giovannecorrea@gmail.com>
2023-11-02 12:26:50 -0300

    workflow deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind(), mdx(), image()],
+	site: 'https://giovannecorrea.github.io',
+	base: '/car_showcase'
 });
